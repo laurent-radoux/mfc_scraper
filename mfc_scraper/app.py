2b5c44fa7f72e5a4c622a6f5ffdf85bfc9efb096
laurent-radoux <50481138+laurent-radoux@users.noreply.github.com>
2021-06-21 12:54:13 +0200

    Added command line options to specify the username, the output folder, whether only new or all figures should be fetched and whether images should be retrieved and dumped

--- mfc_scraper/app.py
+++ mfc_scraper/app.py
@@ -1,6 +1,5 @@
 """Console script for mfc_scraper."""
 import json
-import os
 import sys
 import click
 import os
@@ -10,21 +9,36 @@ from mfc_scraper import scraper
 
 
 @click.command()
-def dump_collection(args=None):
-    figures_data = scraper.get_figures_data(scraper.get_all_figures("Nightzus")[:4])
+@click.option("-u", "--username", required=True)
+@click.option("-o", "--output-folder", default="output")
+@click.option("--new-only/--all", default=True)
+@click.option("--images/--no-images", default=True)
+def dump_collection(username, output_folder, new_only, images):
+    output_folder = f"{output_folder}/{username}"
+    figures_filepath = f"{output_folder}/figures.json"
 
-    figures_filepath = "output/figures.json"
+    all_figures = scraper.get_all_figures(username)
+    already_dumped_figures = []
+    if new_only:
+        with open(figures_filepath, "r") as data_file:
+            already_dumped_figures = json.load(data_file)
+
+    figures_to_load = [f for f in all_figures if f not in [f["id"] for f in already_dumped_figures]]
+    new_figures_data = scraper.get_figures_data(figures_to_load)
+    figures_data = already_dumped_figures + new_figures_data
+
     os.makedirs(os.path.dirname(figures_filepath), exist_ok=True)
     with open(figures_filepath, "w") as data_file:
         json.dump(figures_data, data_file, indent=3)
 
-    for image_data in scraper.get_all_images(figures_data):
-        images_filepath = "output/images/{}_{{}}.jpg".format(image_data['id'])
-        for i, data in enumerate(image_data["images"]):
-            image_filepath = images_filepath.format(i)
-            os.makedirs(os.path.dirname(image_filepath), exist_ok=True)
-            with open(image_filepath, "wb") as data_file:
-                shutil.copyfileobj(data, data_file)
+    if images:
+        for image_data in scraper.get_all_images(new_figures_data):
+            images_filepath = f"{output_folder}/images/{image_data['id']}_{{}}.jpg"
+            for i, data in enumerate(image_data["images"]):
+                image_filepath = images_filepath.format(i)
+                os.makedirs(os.path.dirname(image_filepath), exist_ok=True)
+                with open(image_filepath, "wb") as data_file:
+                    shutil.copyfileobj(data, data_file)
 
     return 0
 
