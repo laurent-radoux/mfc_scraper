880595a4e3f561d19e884600b0c7f86512ae564c
laurent-radoux <50481138+laurent-radoux@users.noreply.github.com>
2021-06-21 15:36:12 +0200

    Renamed app into __main__ to let the package be executed as is

--- tests/test_mfc_scraper.py
+++ tests/test_mfc_scraper.py
@@ -7,7 +7,7 @@ import pytest
 from click.testing import CliRunner
 
 from mfc_scraper import scraper
-from mfc_scraper import app
+from mfc_scraper import __main__
 
 
 @pytest.fixture
@@ -29,9 +29,9 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(app.main)
+    result = runner.invoke(__main__.main)
     assert result.exit_code == 0
     assert 'mfc_scraper.cli.main' in result.output
-    help_result = runner.invoke(app.main, ['--help'])
+    help_result = runner.invoke(__main__.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
